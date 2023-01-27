977b684616d5e6f7335d0bb187efe4e60995976d
YuzuRyo61 <yuzuryo61@yuzulia.work>
2023-01-27 13:07:44 +0900

    Setup Astro adapter and integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
+import node from "@astrojs/node";
+import tailwind from "@astrojs/tailwind";
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  adapter: node({
+    mode: 'standalone'
+  }),
+  integrations: [tailwind(), preact()]
+});
