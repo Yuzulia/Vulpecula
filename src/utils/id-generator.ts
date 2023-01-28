7bf3351161df0a39549af70e2240d28cf11111d2
YuzuRyo61 <yuzuryo61@yuzulia.work>
2023-01-29 00:40:06 +0900

    Prettier

--- src/utils/id-generator.ts
+++ src/utils/id-generator.ts
@@ -1,29 +1,29 @@
 import { ulid, decodeTime } from "ulid";
 
 export class IdGeneratorManager {
-    private constructor(public readonly id: string) {}
+  private constructor(public readonly id: string) {}
 
-    static generate(): IdGeneratorManager {
-        return new IdGeneratorManager(ulid());
+  static generate(): IdGeneratorManager {
+    return new IdGeneratorManager(ulid());
+  }
+
+  static from(newId: string): IdGeneratorManager {
+    const igm = new IdGeneratorManager(newId);
+    if (!igm.validate()) throw new Error("Malformed ID");
+    return igm;
+  }
+
+  private validate(): boolean {
+    try {
+      decodeTime(this.id);
+    } catch (e) {
+      return false;
     }
 
-    static from(newId: string): IdGeneratorManager {
-        const igm = new IdGeneratorManager(newId);
-        if (!igm.validate()) throw new Error('Malformed ID');
-        return igm;
-    }
+    return true;
+  }
 
-    private validate(): boolean {
-        try {
-            decodeTime(this.id);
-        } catch(e) {
-            return false;
-        }
-
-        return true;
-    }
-
-    getDate(): Date {
-        return new Date(decodeTime(this.id));
-    }
+  getDate(): Date {
+    return new Date(decodeTime(this.id));
+  }
 }
